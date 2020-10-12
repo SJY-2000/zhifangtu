81511e166f1957b06f42432f23eae8e89afbf26a
SJY-2000 <2254789143@qq.com>
2020-10-12 19:00:50 +0800

    改

--- zhifangtu/main.cpp
+++ zhifangtu/main.cpp
@@ -3,13 +3,13 @@
 using namespace cv;
 int main()
 {
-	cv::Mat srcMat = imread("G:\\picture\\11.png");
-	cv::Mat dispMat(300, 300, CV_8UC3, Scalar(0, 0, 0));
+	cv::Mat srcMat = imread("G:\\picture\\22.jpg");
+	cv::Mat dispMat(160, 300, CV_8UC3, Scalar(255,255,255));
 	cv::Point pt1; cv::Point pt2; cv::Point pt3; cv::Point pt4;
 	int height = srcMat.rows;
 	int width = srcMat.cols;
-	int tol = height*width;
-	int r = 0; int g = 0; int b = 0; int mul = 250;
+	int r = 0; int g = 0; int b = 0; 
+	int mul = 3000;
 	float histgram_r[256] = { 0 }; float histgram_g[256] = { 0 }; float histgram_b[256] = { 0 };
 	for (int t = 0; t < 256; t++)
 	{
@@ -18,23 +18,20 @@ int main()
 			for (int i = 0; i < width; i++)
 			{
 				if (srcMat.at<Vec3b>(j, i)[0] == t)
-					b++;
+					histgram_b[t]++;
 				if (srcMat.at<Vec3b>(j, i)[1] == t)
-					g++;
+					histgram_g[t]++;
 				if (srcMat.at<Vec3b>(j, i)[2] == t)
-					r++;
+					histgram_r[t]++;
 			}
 		}
-		histgram_r[t] = r / tol;
-		histgram_g[t] = g / tol;
-		histgram_b[t] = b / tol;
-	}
-	for (int k = 0; k < 256; k++)
-	{
-		pt1.x = k; pt1.y = 256;
-		pt2.x = k; pt2.y = 256 - histgram_r[k] * mul;
-		pt3.x = k; pt3.y = 256 - histgram_g[k] * mul;
-		pt4.x = k; pt4.y = 256 - histgram_b[k] * mul;
+		histgram_r[t] = histgram_r[t] / (height*width);
+		histgram_g[t] = histgram_g[t] / (height*width);
+		histgram_b[t] = histgram_b[t] / (height*width);
+		pt1.x = t; pt1.y = 150;
+		pt2.x = t; pt2.y = 150 - histgram_r[t] * mul;
+		pt3.x = t; pt3.y = 150 - histgram_g[t] * mul;
+		pt4.x = t; pt4.y = 150 - histgram_b[t] * mul;
 		line(dispMat, pt1, pt2, CV_RGB(255, 0, 0), 1, 8, 0);
 		line(dispMat, pt1, pt3, CV_RGB(0, 255, 0), 1, 8, 0);
 		line(dispMat, pt1, pt4, CV_RGB(0, 0, 255), 1, 8, 0);
